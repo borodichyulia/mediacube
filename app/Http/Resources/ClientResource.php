191797b59357b76591d6d80eb5966ed497b7642d
yuliaborodich <borodichyulia18@gmail.com>
2025-08-11 12:18:24 +0300

    feat:added resources

---
+++ app/Http/Resources/ClientResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'role' => $this->whenLoaded('clientRole', function () {
+                return [
+                    'id' => $this->clientRole->id,
+                    'name' => $this->clientRole->name,
+                ];
+            }),
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
